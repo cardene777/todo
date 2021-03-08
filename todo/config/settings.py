7f639c388a595064d1b850ac9f57819ce6780662
heku777 <chotips08@gmail.com>
2021-03-09 07:15:52 +0900

    #1 Set up the code.

--- todo/config/settings.py
+++ todo/config/settings.py
@@ -21,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'iz(i9%a#5u6)1k(&a%6@0y1yvy&hrxk3hf&e85+6bzu313s$xl'
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
